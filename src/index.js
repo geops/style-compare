df9f1859478a8eb9cf453f15ed97e435a38cf48c
Olivier Terral <olivier.terral@geops.de>
2020-11-04 17:17:49 +0100

    Add project's code

---
+++ src/index.js
@@ -0,0 +1,36 @@
+/* eslint-disable no-underscore-dangle */
+// Core Dependencies
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { createStore } from 'redux';
+import { Provider } from 'react-redux';
+import reducers from './store/reducers';
+import 'abortcontroller-polyfill/dist/abortcontroller-polyfill-only';
+
+import './i18n';
+
+// Root Component
+import App from './App';
+
+// Others
+import * as serviceWorker from './serviceWorker';
+
+// Store
+const store = createStore(
+  reducers,
+  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__(),
+);
+
+store.subscribe(() => {});
+
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById('root'),
+);
+
+// If you want your app to work offline and load faster, you can change
+// unregister() to register() below. Note this comes with some pitfalls.
+// Learn more about service workers: https://bit.ly/CRA-PWA
+serviceWorker.unregister();
