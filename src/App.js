3abef3214d4ffdde9bb5a4e34c1e79799d6f8339
edellucien <edellucien@gmail.com>
2020-11-06 10:41:49 +0100

    Add zoom and fix input label & placeholder

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
 // Core Functions
 import React from 'react';
 import { Grid, makeStyles } from '@material-ui/core';
+import OLMap from 'ol/Map';
+import { defaults as defaultInteractions } from 'ol/interaction';
+import Zoom from 'react-spatial/components/Zoom';
 import Map from './components/Map';
 
-import './App.css';
+import './App.sass';
 
 const useStyles = makeStyles(() => ({
   container: { height: '100%' },
@@ -14,14 +17,23 @@ const useStyles = makeStyles(() => ({
 export default function App() {
   const classes = useStyles();
 
+  const map = new OLMap({
+    controls: [],
+    interactions: defaultInteractions({
+      altShiftDragRotate: false,
+      pinchRotate: false,
+    }),
+  });
+
   return (
     <Grid container className={classes.container}>
       <Grid item xs={6} className={classes.mapLeft}>
         <Map permalinkParam="left" />
       </Grid>
       <Grid item xs={6} className={classes.mapRight}>
-        <Map permalinkParam="right" />
+        <Map permalinkParam="right" map={map} />
       </Grid>
+      <Zoom map={map} />
     </Grid>
   );
 }
--- src/components/Map/Map.js
+++ src/components/Map/Map.js
@@ -1,14 +1,14 @@
 import React, { useState, useMemo } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
 import PropTypes from 'prop-types';
+import OLMap from 'ol/Map';
 import { MapboxLayer } from 'mobility-toolbox-js/ol';
 import BasicMap from 'react-spatial/components/BasicMap';
-import { TextField, makeStyles } from '@material-ui/core';
+import { FormLabel, TextField, makeStyles } from '@material-ui/core';
 import qs from 'query-string';
 import { setCenter, setZoom } from '../../store/actions';
 
 // Styles
-import './Map.sass';
 import 'react-spatial/themes/default/index.scss';
 import 'ol/ol.css';
 
@@ -17,24 +17,30 @@ const useStyles = makeStyles(() => ({
     width: '100%',
     height: '100%',
   },
-  input: {
+  inputWrapper: {
+    padding: 15,
+    background: 'white',
     position: 'absolute',
     top: 0,
     left: 0,
     right: 0,
     margin: 'auto',
-    background: 'white',
-    padding: 15,
+  },
+  label: {
+    paddingLeft: '5px',
+  },
+  input: {
+    width: '100%',
   },
 }));
 
-const Map = ({ permalinkParam }) => {
+const Map = ({ map, permalinkParam }) => {
   // Store
   const center = useSelector((state) => state.center);
   const zoom = useSelector((state) => state.zoom);
+  const [focused, onFocused] = useState(false);
   const [tilesUrl, setTilesUrl] = useState(
-    qs.parse(window.location.search)[permalinkParam] ||
-      `https://maps.geops.io/styles/base_bright_v2/style.json`,
+    qs.parse(window.location.search)[permalinkParam] || '',
   );
   const dispatch = useDispatch();
   const classes = useStyles();
@@ -58,6 +64,7 @@ const Map = ({ permalinkParam }) => {
   return (
     <>
       <BasicMap
+        map={map}
         className={classes.map}
         layers={[baseLayer]}
         center={center}
@@ -67,23 +74,34 @@ const Map = ({ permalinkParam }) => {
           dispatch(setZoom(evt.map.getView().getZoom()));
         }}
       />
-      <TextField
-        label="Tiles url"
-        className={classes.input}
-        variant="outlined"
-        value={tilesUrl}
-        onChange={(evt) => {
-          setTilesUrl(evt.target.value);
-        }}
-      />
+      <div className={classes.inputWrapper}>
+        <FormLabel focused={focused} className={classes.label}>
+          GL Style URL
+        </FormLabel>
+        <TextField
+          className={classes.input}
+          onFocus={() => onFocused(true)}
+          onBlur={() => onFocused(false)}
+          placeholder="Paste the URL to a GL Style JSON here"
+          variant="outlined"
+          value={tilesUrl}
+          onChange={(evt) => {
+            setTilesUrl(evt.target.value);
+          }}
+        />
+      </div>
     </>
   );
 };
 
 Map.propTypes = {
   permalinkParam: PropTypes.string,
+  map: PropTypes.instanceOf(OLMap),
 };
 
-Map.defaultProps = { permalinkParam: 'left' };
+Map.defaultProps = {
+  permalinkParam: 'left',
+  map: null,
+};
 
 export default Map;
